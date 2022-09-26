ca345682600764baac29ddd9d078888bff610751
PankratovEP <enkwi@mail.ru>
2022-09-27 00:19:19 +0300

    Добавил поиск ссылок в html файлах

--- about_internet.py
+++ about_internet.py
@@ -1,13 +1,10 @@
-'''while True:
-    znach=float(input('Задайте коэф.усиления = '))
-    if znach<17.5 or znach>23.8:
-        print('Ошибка!')
-    else:
-        break'''
-
+from re import *
 import requests
-res = requests.get('https://docs.python.org/3.5/')
-print(res.status_code)
-print(res.headers['Content-Type'])
-print(res.content)
-print(res.text)
+first, sec = input(), input()
+spisok = findall(r'a href="(http.*)">', requests.get(first).text)
+for i in spisok:
+    buf = requests.get(i)
+    if sec in buf.text:
+        exit(print('Yes'))
+print('No')
+
